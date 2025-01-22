7f0926b0430a4e500b56a0e76d90a59338c6c05c
Dede <111243030+dedetheprogrammer@users.noreply.github.com>
2025-01-22 13:00:44 +0100

    Probatina para el Intellisense + carpeta Scripts

---
+++ Project GGJ 2025/Assets/Scripts/CharacterRbController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CharacterRbController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameObject gameObject = new GameObject();
+        gameObject.transform.position = Vector3.one;
+        // Esto es un Script de prueba para el intelissense equisd
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
